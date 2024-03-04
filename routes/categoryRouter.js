bee754adb943d07d84868aff3e77d04b64e4a34a
PenVsse <vusiphu20042000@gmail.com>
2024-03-04 17:06:05 +0700

    BE die r

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -6,7 +6,7 @@ const { isLogin, isAdmin } = require('../middleware/loginChecker');
 categoryRouter.post("/add-category", isLogin, isAdmin, categoryController.addCategory);
 categoryRouter.get("/get-all-categories", isLogin, categoryController.getAllCategories);
 categoryRouter.put("/update-category", isLogin, isAdmin, categoryController.updateCategory);
-categoryRouter.delete("/delete-category/:id", isLogin, isAdmin, categoryController.deleteCategory);
-categoryRouter.delete("/delete-category", isLogin, isAdmin, categoryController.deleteCategory);
+categoryRouter.delete("/delete-category/:id", isLogin, isAdmin, categoryController.deleteCategory); 
+categoryRouter.delete("/delete-category", isLogin, isAdmin, categoryController.deleteCategory); 
 
 module.exports = categoryRouter;
